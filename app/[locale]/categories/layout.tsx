92ba74043423fd8714ab11549e84307eda836b18
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-11-20 20:21:19 +0200

    add fix in category layout

--- app/[locale]/categories/layout.tsx
+++ app/[locale]/categories/layout.tsx
@@ -1,45 +1,16 @@
-import React, { ReactNode } from 'react';
-import Head from 'next/head';
 import Header from '@/app/UI/organisms/Header';
+import type { Metadata } from 'next';
 
-interface LayoutProps {
-  children: ReactNode;
-  title?: string;
-  description?: string;
-  keywords?: string;
-}
+export const metadata: Metadata = {
+  title: 'Categories',
+  description: 'This is a Products Categories',
+};
 
-const Layout: React.FC<LayoutProps> = ({
-  children,
-  title = 'Categories',
-  description = 'This is a Products Categories',
-  keywords = 'Categories, Products, Game, Electronics',
-}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Head>
-        {/* Basic SEO */}
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* Open Graph / Social Media Metadata */}
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://virunga.vercel.app" />
-        <meta property="og:image" content="https://virunga.vercel.app/og-image.png" />
-
-        {/* Twitter Metadata */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="https://virunga.vercel.app/twitter-image.png" />
-      </Head>
       <Header />
       <main className="flex-grow p-4">{children}</main>
-    
     </div>
   );
 };
